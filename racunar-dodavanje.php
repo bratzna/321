986d8abc18a6204e2ea117b4a70f08fd25361254
bratzna <42917284+bratzna@users.noreply.github.com>
2018-09-03 03:50:15 +0200

    Create racunar-dodavanje.php

---
+++ racunar-dodavanje.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dodavanje racunara</title>
+</head>
+<body>
+    <h1>Dodavanje racunara</h1>
+    <form action="racunar-dodavanje-db.php" method="post">
+        Opis: <input type="text" name="opis" />
+        <hr />
+        <button type="submit">Dodavanje</button>
+    </form>
+</body>
+</html>
