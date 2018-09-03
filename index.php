9b5579f65653b63d0e3b4b06b87ba7ef14e8d5c7
bratzna <42917284+bratzna@users.noreply.github.com>
2018-09-03 03:44:38 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <a href="racunar-pregled.php">Pregled racunara</a>
+    <a href="komponenta-pregled.php">Pregled komponenti</a>
+</body>
+</html>
